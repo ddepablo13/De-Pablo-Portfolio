e9a19b3ee21ce07f890c834113c37722afde1393
Daniel De Pablo <ddepablo@miners.utep.edu>
2024-08-05 22:56:42 -0600

    added gifs, cleaned up code, and added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function to toggle the hamburger menu
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamlurger-icon");
